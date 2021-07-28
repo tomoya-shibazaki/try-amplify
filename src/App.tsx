cf5bc25a38a087ab720a88b534e3b787922ee8a0
tomoya shibazaki <tshibazaki8165@gmail.com>
2021-07-28 17:28:43 +0900

    recreate auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,34 @@
+import React, {useEffect} from 'react';
+import Amplify from "aws-amplify";
+import {AmplifyAuthenticator, AmplifySignOut} from '@aws-amplify/ui-react';
+import {AuthState, onAuthUIStateChange} from '@aws-amplify/ui-components';
+import awsmobile from './aws-exports';
 import EducationPlans from './EducationPlans'
 import Form from './Form'
 
 
-const App = () => {
+Amplify.configure(awsmobile);
 
-  return (
+const App = () => {
+  const [authState, setAuthState] = React.useState<AuthState>();
+
+  useEffect(() => {
+    return onAuthUIStateChange((nextAuthState, authData) => {
+      setAuthState(nextAuthState);
+    });
+  }, [])
+
+  return authState === AuthState.SignedIn ? (
     <div className="App">
       <EducationPlans />
       <br />
       <hr />
       <br />
       <Form />
+      <AmplifySignOut/>
     </div>
+  ) : (
+    <AmplifyAuthenticator/>
   )
 }
 
